506f523c4fd357e09036baf5b75e0683481502cf
samarthch79-art <samarthch79@gmail.com>
2026-03-01 17:05:58 +0530

    brute force solution for maximum subarray problem
    Implement brute force method to find maximum subarray sum.

---
+++ 13_maximum_subarray_bruteforce.py
@@ -0,0 +1,16 @@
+# finding the maximum subarray sum
+# bruteforce approach:
+
+a=[1,2,5,3,-5,2,1]
+max=float("-inf")
+sum=0
+for i in range(len(a)):
+    for j in range(i,len(a)):
+        sum+=a[j]
+        if sum>max:
+            max=sum    
+    sum=0
+print(max)  
+
+# time complexity O(n)
+#cspace complexity O(1)
